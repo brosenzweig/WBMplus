37c9b3fbeb4e403b4d45992f20267fbbab764044
bmfekete <bmfekete@gmail.com>
2007-07-23 18:11:02 +0000

    Iterating further on the MacOSX Makefiles.

--- common.mk
+++ common.mk
@@ -1,16 +1,14 @@
 UNIX=$(shell uname)
 
 ifeq ($(UNIX),Linux)
-export MATHLIB=-lm
-export UNIXAR=ar -ru
 export UNIXCC=gcc
 export UNIXCCOPS=-Wall -fsigned-char -D_GNU_SOURCE
+export UNIXLIBS=-lm
 export UNIXMAKE=make
 endif
 ifeq ($(UNIX),Darwin)
-export MATHLIB=-lm
-export UNIXAR=ar -ru
 export UNIXCC=gcc
 export UNIXCCOPS=-Wall -fsigned-char -D_GNU_SOURCE
+export UNIXLIBS=-lm
 export UNIXMAKE=make
 endif
