312f0e51ba677816c68e5f4440a799885b242ed7
bmfekete <bmfekete@gmail.com>
2008-05-20 16:02:25 +0000

    Mistakenly copied CMlib.

--- CMlib/src/cmMath.c
+++
@@ -1,7 +0,0 @@
-#include <math.h>
-#include <cm.h>
-
-bool CMmathEqualValues (double var0,double var1) {
-	if (fabs (var0) + fabs (var1) == (double) 0.0) return (true);
-	return (fabs (var0 - var1) / (fabs (var0) + fabs (var1)) < CMmathEpsilon);
-}
