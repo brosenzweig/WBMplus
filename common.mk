16c94ca813118ea90cde11ab2ba7e26944e3abd5
bmfekete <bmfekete@gmail.com>
2009-10-03 00:35:45 +0000

    Fixing Makefile for pthread.

--- common.mk
+++ common.mk
@@ -2,20 +2,20 @@ UNIX=$(shell uname)
 
 ifeq ($(UNIX),Linux)
 export UNIXCC=gcc
-export UNIXCCOPS=-Wall -fsigned-char -D_GNU_SOURCE
-export UNIXLIBS=-lm
+export UNIXCCOPS=-g -Wall -pthread -fsigned-char -D_GNU_SOURCE
+export UNIXLIBS=-pthread -lm
 export UNIXMAKE=make
 endif
 ifeq ($(UNIX),Darwin)
 export UNIXCC=gcc
-export UNIXCCOPS=-Wall -fsigned-char -D_GNU_SOURCE
-export UNIXLIBS=-lm
+export UNIXCCOPS=-g -Wall -pthread -fsigned-char -D_GNU_SOURCE
+export UNIXLIBS=-pthread -lm
 export UNIXMAKE=make
 endif
 ifeq ($(UNIX),SunOS)
 export UNIXAR=ar -ru
 export UNIXCC=gcc
-export UNIXCCOPS=-g -Wall -fsigned-char -D_GNU_SOURCE
-export UNIXLIBS=-lm
-export UNIXMAKE=make
+export UNIXCCOPS=-g -Wall -pthread -fsigned-char -D_GNU_SOURCE
+export UNIXLIBS=-pthread -lm
+export UNIXMAKE=gmake
 endif
