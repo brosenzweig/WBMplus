b9a3ce78795236ed34c4441d36ae9505daf6f7c8
bmfekete <bmfekete@gmail.com>
2011-06-07 16:22:43 +0000

    Revising Makefile's common.mk to allow CUSTOM_INC and CUSTOM_LIB specification for non-standard platforms.

--- common.mk
+++ common.mk
@@ -1,21 +1,6 @@
 UNIX=$(shell uname)
 
-ifeq ($(UNIX),Linux)
 export UNIXCC=gcc
 export UNIXCCOPS=-g -Wall -pthread -fsigned-char -D_GNU_SOURCE
 export UNIXLIBS=-pthread -lm
 export UNIXMAKE=make
-endif
-ifeq ($(UNIX),Darwin)
-export UNIXCC=gcc
-export UNIXCCOPS=-g -Wall -pthread -fsigned-char -D_GNU_SOURCE
-export UNIXLIBS=-pthread -lm
-export UNIXMAKE=make
-endif
-ifeq ($(UNIX),SunOS)
-export UNIXAR=ar -ru
-export UNIXCC=gcc
-export UNIXCCOPS=-g -Wall -pthread -fsigned-char -D_GNU_SOURCE
-export UNIXLIBS=-pthread -lm
-export UNIXMAKE=gmake
-endif
